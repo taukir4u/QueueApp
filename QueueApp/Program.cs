8aaa834fd2691aab7c0f96033695782c8587ea69
taukir4u <taukir4u@gmail.com>
2014-12-26 20:38:36 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ QueueApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> aQueue = new Queue<int>();
+            aQueue.Enqueue(100);
+            aQueue.Enqueue(200);
+            aQueue.Enqueue(150);
+
+            foreach (var queueValue in aQueue)
+            {
+                Console.WriteLine(queueValue);
+            }
+            //Console.WriteLine(aQueue.Dequeue());
+            //Console.WriteLine(aQueue.Dequeue());
+            //Console.WriteLine(aQueue.Dequeue());
+
+
+            Console.WriteLine("Queue Count: " +aQueue.Count);
+            Console.ReadKey();
+
+        }
+    }
+}
